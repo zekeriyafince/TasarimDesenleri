214c63c6bf14be024be9035b5681b984cf88a3a8
Zekeriya İnce <mhdszekeriya@gmail.com>
2019-01-19 14:31:47 +0300

    Update Main.java

--- singleton/Main.java
+++ singleton/Main.java
@@ -9,11 +9,6 @@ public class Main {
     /*
      * Sıngleton Design Pattern 
      * AMAC: Bir nesnenin calisma zamanında tek olmasını istemek
-     * Bir sınıfının instance orneginin tek olmasını istemektir.
-     * Bunun icinde Constructor metodu private tanımlanmalıdır.Boylece diger sınıflardan
-    new operatoru ile nesne ornegi yaratamayacak.
-     * Bundan sonra mecburen nesne uretilmesini kendi icinde uretmesi lazım.
-     * Uretilen instance dısarıya Singleton tipinde public static bir metod ile verilir.
      * 
     */
     public static void main(String[] args) {
